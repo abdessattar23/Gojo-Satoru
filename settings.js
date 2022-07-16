38f6750f50ea006efecc4f63442bb3c19e166dc9
abdessattar23 <106479409+abdessattar23@users.noreply.github.com>
2022-07-16 22:35:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.owner = ['212658585324']
+global.premium = ['212658585324']
+global.ownernomer = '212658585324'
+global.ownername = 'Abdessattar'
+global.botname = 'Abdessattar'
+global.footer = '©AbdelDroidInc.'
+global.ig = 'https://github.com/abdessattar23'
+global.region = 'Morocco,Fez'
+global.sc = 'https://github.com/abdessattar23/Gojo-Satoru'
+global.myweb = 'https://instagram.com/abdessattar23'
+global.packname = 'Abdessattar'
+global.author = 'Abdessattar'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
